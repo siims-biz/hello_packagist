de609e15f6b1bfe57caa7da8c32524bb09b524bd
matjung <matjung@hotmail.com>
2023-04-08 15:10:54 +0200

    Replaced PackageEvent with Event

--- src/HelloPackagist.php
+++ src/HelloPackagist.php
@@ -5,11 +5,12 @@ use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 
 class HelloPackagist {
-    public static function postPackageInstall(PackageEvent $event)
+    //public static function postPackageInstall(PackageEvent $event)
+    public static function postPackageInstall(Event $event)
     {
-        $installedPackage = $event->getOperation()->getPackage();
+//        $installedPackage = $event->getOperation()->getPackage();
         // do stuff
-        print_r($installedPackage);
+  echo "postPackageInstall";
     }
 
     public static function postPackageUpdate(Event $event)
